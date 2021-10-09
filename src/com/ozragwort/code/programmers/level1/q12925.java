562c88601864c4499e6e0c2645d488a46eabb74f
OzRagwort <ozragwort@gmail.com>
2021-10-10 01:40:19 +0900

    Add programmers clear 1

---
+++ src/com/ozragwort/code/programmers/level1/q12925.java
@@ -0,0 +1,8 @@
+package com.ozragwort.code.programmers.level1;
+
+public class q12925 {
+    public int solution(String s) {
+        int answer = Integer.parseInt(s);
+        return answer;
+    }
+}
