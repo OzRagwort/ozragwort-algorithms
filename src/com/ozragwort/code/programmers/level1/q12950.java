983bd4d54996f3056164e4225e15d9e8419ef207
OzRagwort <ozragwort@gmail.com>
2021-10-24 20:12:00 +0900

    Add programmers clear 1

---
+++ src/com/ozragwort/code/programmers/level1/q12950.java
@@ -0,0 +1,18 @@
+package com.ozragwort.code.programmers.level1;
+
+public class q12950 {
+    public int[][] solution(int[][] arr1, int[][] arr2) {
+        int x = arr1.length;
+        int y = arr1[0].length;
+
+        int[][] answer = new int[x][y];
+
+        for (int i = 0 ; i < x ; i++) {
+            for (int j = 0; j < y; j++) {
+                answer[i][j] = arr1[i][j] + arr2[i][j];
+            }
+        }
+
+        return answer;
+    }
+}
